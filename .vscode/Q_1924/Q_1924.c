2fb950f268231c9b4edbce2a0df0cf9f60e4a8b3
GARAM-SHIN <rkfka4291@naver.com>
2026-01-31 22:55:53 +0900

    2026-01-31

---
+++ .vscode/Q_1924/Q_1924.c
@@ -0,0 +1,21 @@
+// 2007년
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    const char *date[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+    
+    scanf("%d %d", &x, &y);
+
+    for(int i=0; i<x-1; i++)
+    {
+        y += month[i];
+    }
+
+    printf("%s\n", date[y % 7]);
+
+    return 0;
+}
